6275567e2629faeb03714683e01d8bd712352ef5
Deniston A <72125885+Deniston2K@users.noreply.github.com>
2020-12-04 22:10:32 +0530

    Delete Color image to greyscale.py

--- Color image to greyscale.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-data=cv2.imread(r"E:\KCT\Semester 5\RAC\RAC 2020\RAC Dec\Lectures\OpenCV Javid Hussain\Lecture 1\Open CV Images\kick.jpg")
-grey=cv2.cvtColor(data,cv2.COLOR_BGR2RGB)
-cv2.imshow('image',grey)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
